c3eedd5467d793f262a78b32b3c91073d0d5f1ef
Revision <tkn963@gmail.com>
2022-01-19 04:33:58 +0100

    Gossip options
    Removed the script that deletes gossip menu options since it causes unnecessary errors. Added them as conditions instead, and added conditions to disable shaman trainers for alliance and paladin trainers for horde.

--- World/gossip_menu_option.sql
+++
@@ -1,4 +0,0 @@
-DELETE FROM `gossip_menu_option` WHERE `OptionText` LIKE '%Dual Talent%';
-DELETE FROM `gossip_menu_option` WHERE `OptionText` LIKE '%Jewelcrafting%';
-DELETE FROM `gossip_menu_option` WHERE `OptionText` LIKE '%Inscription%';
-DELETE FROM `gossip_menu_option` WHERE `OptionText` LIKE '%Lexicon of Power%';
