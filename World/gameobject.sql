e3e7190f6f0fdfff9e60e08a070448ab33c7c1b7
Revision <tkn963@gmail.com>
2022-01-19 01:24:14 +0100

    Joined
    Joined a couple queries together. I will begin work on creatures, starting with their names and then their levels etc.

--- World/gameobject.sql
+++ World/gameobject.sql
@@ -1,5 +1,5 @@
 -- Outland Map
-DELETE FROM `gameobject` WHERE `id`=181310 AND `map`=0;
+DELETE FROM `gameobject` WHERE `id`=181310;
 -- Temporal Prison
 DELETE FROM `gameobject` WHERE `id`=184006;
 -- Guild Vault
@@ -7,8 +7,6 @@ DELETE FROM `gameobject` WHERE `id` IN (187290, 187292, 187294, 187295, 187296, 
 -- Lexicon of Power
 DELETE FROM `gameobject` WHERE `id`=193981;
 -- Portal to Blasted Lands
-DELETE FROM `gameobject` WHERE `id`=195141;
--- Portal to Blasted Lands
-DELETE FROM `gameobject` WHERE `id`=195142;
+DELETE FROM `gameobject` WHERE `id` IN (195141, 195142);
 -- Meeting Stone - Karazhan
 DELETE FROM `gameobject` WHERE `id`=184463;
