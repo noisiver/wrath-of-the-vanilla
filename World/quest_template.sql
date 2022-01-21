057878c4f943f67f80a543e97282771dfbc8aa8c
Revision <tkn963@gmail.com>
2022-01-21 20:29:32 +0100

    Stormwind City
    Removed the individual deletes of quest poi entries. Clearing the entire tables instead since the results are the same.
    
    Changed a quest in Stormwind to fail if the player releases their spirit. I tested the quest but it doesn't seem to work properly.
    
    The profession changes also caused some DB errors that I need to look at.

--- World/quest_template.sql
+++ World/quest_template.sql
@@ -19,6 +19,9 @@ UPDATE `quest_template` SET `Flags`=0, `LogDescription`='Bring 8 pieces of Tough
 -- Report to Goldshire
 UPDATE `quest_template` SET `Flags`=0 WHERE `ID`=54;
 
+-- A Meal Served Cold
+UPDATE `quest_template` SET `Flags`=9, `LogDescription`='Kill a Cold Eye Basilisk, get a Chilled Basilisk Haunch, and return it to Angus Stern in the Blue Recluse.$B$B<You must not release your spirit to succeed in this quest.>' WHERE `ID`=212;
+
 -- A Threat Within
 UPDATE `quest_template` SET `Flags`=0 WHERE `ID`=783;
 
