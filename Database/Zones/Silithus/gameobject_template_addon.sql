36a79273c2b869199fcf73e8144c6f6e649ae405
Revision <tkn963@gmail.com>
2022-01-29 23:41:13 +0100

    Merge
    I decided that it was easier to merge everything into one set of folders instead of having them split up. There are a few left that I need to sort still.
    
    I also added the AQ Gate to silithus. I do plan to try my best to port the gate opening event over to azerothcore, but that's a long way down the road.

---
+++ Database/Zones/Silithus/gameobject_template_addon.sql
@@ -0,0 +1,8 @@
+-- Gate of Ahn'Qiraj
+UPDATE `gameobject_template_addon` SET `flags`=4 WHERE `entry`=176146;
+
+-- Ahn'Qiraj Gate Roots
+UPDATE `gameobject_template_addon` SET `flags`=4 WHERE `entry`=176147;
+
+-- Ahn'Qiraj Gate Runes
+UPDATE `gameobject_template_addon` SET `flags`=4 WHERE `entry`=176148;
