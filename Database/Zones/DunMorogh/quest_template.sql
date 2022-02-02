4cd60f6e42da34f7d0ff53a0e3d6c4198f298ebe
Revision <tkn963@gmail.com>
2022-02-02 18:02:05 +0100

    Fixed
    Removed quests that are only there to change their flag but are either 0 by default or 8 (sharable).
    Added the flag 8 (sharable) where it was previously set to 0 but should be sharable.

--- Database/Zones/DunMorogh/quest_template.sql
+++ Database/Zones/DunMorogh/quest_template.sql
@@ -1,2 +1,2 @@
 -- Beginnings (Warlock)
-UPDATE `quest_template` SET `Flags`=0 WHERE `ID`=1599;
+UPDATE `quest_template` SET `Flags`=8 WHERE `ID`=1599;
--- Database/Zones/Durotar/quest_template.sql
+++ Database/Zones/Durotar/quest_template.sql
@@ -1,26 +1,26 @@
 -- Cutting Teeth
-UPDATE `quest_template` SET `Flags`=0, `LogDescription`='Kill 10 Mottled Boars then return to Gornek at the Den.', `RequiredNpcOrGoCount1`=10 WHERE `ID`=788;
+UPDATE `quest_template` SET `Flags`=8, `LogDescription`='Kill 10 Mottled Boars then return to Gornek at the Den.', `RequiredNpcOrGoCount1`=10 WHERE `ID`=788;
 
 -- Sting of the Scorpid
-UPDATE `quest_template` SET `Flags`=0, `LogDescription`='Get 10 Scorpid Worker Tails for Gornek in the Den.', `RequiredItemCount1`=10 WHERE `ID`=789;
+UPDATE `quest_template` SET `Flags`=8, `LogDescription`='Get 10 Scorpid Worker Tails for Gornek in the Den.', `RequiredItemCount1`=10 WHERE `ID`=789;
 
 -- Sarkoth
-UPDATE `quest_template` SET `Flags`=0 WHERE `ID`=790;
+UPDATE `quest_template` SET `Flags`=8 WHERE `ID`=790;
 
 -- Vile Familiars
-UPDATE `quest_template` SET `Flags`=0, `LogDescription`='Kill 12 Vile Familiars.$b$bReturn to Zureetha Fargaze outside the Den.', `RequiredNpcOrGoCount1`=12 WHERE `ID`=792;
+UPDATE `quest_template` SET `Flags`=8, `LogDescription`='Kill 12 Vile Familiars.$b$bReturn to Zureetha Fargaze outside the Den.', `RequiredNpcOrGoCount1`=12 WHERE `ID`=792;
 
 -- Burning Blade Medallion
-UPDATE `quest_template` SET `Flags`=0 WHERE `ID`=794;
+UPDATE `quest_template` SET `Flags`=8 WHERE `ID`=794;
 
 -- Sarkoth
 UPDATE `quest_template` SET `Flags`=0 WHERE `ID`=804;
 
 -- Report to Sen'jin Village
-UPDATE `quest_template` SET `Flags`=0 WHERE `ID`=805;
+UPDATE `quest_template` SET `Flags`=8 WHERE `ID`=805;
 
 -- Call of Earth (Shaman)
-UPDATE `quest_template` SET `Flags`=0 WHERE `ID`=1516;
+UPDATE `quest_template` SET `Flags`=8 WHERE `ID`=1516;
 
 -- Call of Earth (Shaman)
 UPDATE `quest_template` SET `Flags`=0 WHERE `ID`=1517;
@@ -28,17 +28,14 @@ UPDATE `quest_template` SET `Flags`=0 WHERE `ID`=1517;
 -- A Peon's Burden
 UPDATE `quest_template` SET `Flags`=0 WHERE `ID`=2161;
 
--- Simple Parchment
-UPDATE `quest_template` SET `Flags`=0 WHERE `ID`=2383;
-
 -- Galgar's Cactus Apple Surprise
-UPDATE `quest_template` SET `Flags`=0, `LogDescription`='Bring Galgar 10 Cactus Apples. You remember him saying that they could be found near cactuses.', `RequiredItemCount1`=10 WHERE `ID`=4402;
+UPDATE `quest_template` SET `Flags`=8, `LogDescription`='Bring Galgar 10 Cactus Apples. You remember him saying that they could be found near cactuses.', `RequiredItemCount1`=10 WHERE `ID`=4402;
 
 -- Your Place In The World
-UPDATE `quest_template` SET `Flags`=0 WHERE `ID`=4641;
+UPDATE `quest_template` SET `Flags`=8 WHERE `ID`=4641;
 
 -- Lazy Peons
 UPDATE `quest_template` SET `Flags`=0 WHERE `ID`=5441;
 
 -- Thazz'ril's Pick
-UPDATE `quest_template` SET `Flags`=0 WHERE `ID`=6394;
+UPDATE `quest_template` SET `Flags`=8 WHERE `ID`=6394;
