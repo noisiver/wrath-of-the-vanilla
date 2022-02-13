788fa30d1f07acb08f7fb95b9fee8b77b0f69235
Revision <tkn963@gmail.com>
2022-02-13 23:39:18 +0100

    Class Spells
    Reworked the class spells to make sure no entries are left untouched.

--- Database/Player/npc_trainer.sql
+++ Database/Player/npc_trainer.sql
@@ -1,32 +1,18 @@
--- Warrior Trainer
-DELETE FROM `npc_trainer` WHERE `ID` IN (200001, 200002) AND `ReqLevel` > 60;
+-- Global
+DELETE FROM `npc_trainer` WHERE `ID` IN (200001, 200002, 200003, 200004, 200005, 200006, 200007, 200008, 200009, 200010, 200011, 200012, 200013, 200014, 200015, 200016, 200017, 200018, 200020, 200021) AND `ReqLevel` > 60;
 
--- Paladin Trainer
-DELETE FROM `npc_trainer` WHERE `ID` IN (200004, 200020, 200021) AND `ReqLevel` > 60;
+-- Paladin
 DELETE FROM `npc_trainer` WHERE `SpellID` IN (13820, 23214, 34767, 34768);
 
--- Hunter Trainer
-DELETE FROM `npc_trainer` WHERE `ID`=200014 AND `ReqLevel` > 60;
-
--- Rogue
-DELETE FROM `npc_trainer` WHERE `ID`=200016 AND `ReqLevel` > 60;
-
 -- Priest
-DELETE FROM `npc_trainer` WHERE `ID`=200012 AND `ReqLevel` > 60;
 DELETE FROM `npc_trainer` WHERE `SpellID` IN (21562, 21564, 27681, 27683);
 
--- Shaman
-DELETE FROM `npc_trainer` WHERE `ID`=200018 AND `ReqLevel` > 60;
-
 -- Mage
-DELETE FROM `npc_trainer` WHERE `ID`=200008 AND `ReqLevel` > 60;
 DELETE FROM `npc_trainer` WHERE `SpellID` IN (10140, 23028, 27127, 43002);
 
 -- Warlock
-DELETE FROM `npc_trainer` WHERE `ID` IN (200010, 200009) AND `ReqLevel` > 60;
 DELETE FROM `npc_trainer` WHERE `SpellID` IN (1710, 23161);
 
 -- Druid
-DELETE FROM `npc_trainer` WHERE `ID`=200006 AND `ReqLevel` > 60;
 DELETE FROM `npc_trainer` WHERE `SpellID` IN (33950, 40120);
 DELETE FROM `npc_trainer` WHERE `SpellID` IN (21849, 21850);
